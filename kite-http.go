4e34b962607629fbb40a7882b2b6cbbaebce2a69
Andy Stewart <boss@airbladesoftware.com>
2018-03-16 11:36:05 +0000

    Add support for headers.

--- kite-http.go
+++ kite-http.go
@@ -1,6 +1,8 @@
 // USAGE
 //
-//   kite-http [--post --data DATA [--debug]] <url>
+//   kite-http [--header name=value] [--post --data DATA [--debug]] <url>
+//
+// Sets a default content-type of "application/x-www-form-urlencoded" for POSTs.
 //
 // Writes the http response (including header's protocol and status) to stdout.
 //
@@ -19,7 +21,24 @@ import (
 	"time"
 )
 
+type headers map[string]string
+
+var myHeaders headers
+
+func (i *headers) String() string {
+	return fmt.Sprint(*i)
+}
+
+func (i *headers) Set(value string) error {
+	header := strings.SplitN(value, "=", 2)
+	myHeaders[header[0]] = header[1]
+	return nil
+}
+
 func main() {
+	myHeaders = make(map[string]string)
+	flag.Var(&myHeaders, "header", "HTTP header as name=value")
+
 	debug := flag.Bool("debug", false, "write --data arg to stderr")
 	isPost := flag.Bool("post", false, "POST request")
 	data := flag.String("data", "", "data to send to the HTTP server")
@@ -36,6 +55,7 @@ func main() {
 		io.WriteString(os.Stderr, *data+"\n")
 	}
 
+	var req *http.Request
 	var resp *http.Response
 	var err error
 
@@ -44,10 +64,23 @@ func main() {
 	}
 
 	if *isPost {
-		resp, err = client.Post(url, "application/x-www-form-urlencoded", strings.NewReader(*data)) // match curl's content-type
+		req, err = http.NewRequest("POST", url, strings.NewReader(*data))
 	} else {
-		resp, err = client.Get(url)
+		req, err = http.NewRequest("GET", url, nil)
 	}
+
+	for k, v := range myHeaders {
+		req.Header.Set(k, v)
+	}
+
+	if *isPost {
+		_, ok := myHeaders["Content-Type"]
+		if !ok {
+			req.Header.Set("Content-Type", "application/x-www-form-urlencoded")
+		}
+	}
+
+	resp, err = client.Do(req)
 
 	if err != nil {
 		log.Fatal(err)
